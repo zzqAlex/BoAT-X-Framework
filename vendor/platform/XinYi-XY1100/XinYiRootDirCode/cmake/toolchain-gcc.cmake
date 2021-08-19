a3e600d66899559ed60fbb2ec410053a9ecc57c0
zzqAlex <18621758039@163.com>
2021-08-19 20:20:29 +0800

    add support for XinYin-XY1100 module,and can compile & run a simple demo

---
+++ vendor/platform/XinYi-XY1100/XinYiRootDirCode/cmake/toolchain-gcc.cmake
@@ -0,0 +1,4 @@
+
+    set(libbw_file_name ${CMAKE_CURRENT_SOURCE_DIR}/BoAT-X-Framework/lib/libboatwallet.a)
+    set(libbh_file_name ${CMAKE_CURRENT_SOURCE_DIR}/BoAT-X-Framework/lib/libboatvendor.a)
+
